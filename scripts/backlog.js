e4123d78c015e50e33bf0e9b89ebee150afd92f2
MertzAndreas <SG82DA@student.aau.dk>
2023-04-01 15:54:31 +0200

    Changes to Backlog theme and functionality

--- scripts/backlog.js
+++ scripts/backlog.js
@@ -26,6 +26,8 @@ function createTasks(tasks) {
 
   // Create table header
   const headerRow = document.createElement("tr");
+  const indexHeader = document.createElement("th");
+  indexHeader.textContent = "Index";
   const taskNameHeader = document.createElement("th");
   taskNameHeader.textContent = "TaskName";
   const assigneeHeader = document.createElement("th");
@@ -36,16 +38,22 @@ function createTasks(tasks) {
   endDateHeader.textContent = "End Date";
   const statusHeader = document.createElement("th");
   statusHeader.textContent = "Status";
+  const priorityHeader = document.createElement("th");
+  priorityHeader.textContent = "Priority";
+  headerRow.appendChild(indexHeader);
   headerRow.appendChild(taskNameHeader);
   headerRow.appendChild(assigneeHeader);
   headerRow.appendChild(startDateHeader);
   headerRow.appendChild(endDateHeader);
   headerRow.appendChild(statusHeader);
+  headerRow.appendChild(priorityHeader);
   table.appendChild(headerRow);
 
   // Create table rows
   tasks.forEach((task, index) => {
     const row = document.createElement("tr");
+    const taskindex = document.createElement("td");
+    taskindex.textContent = index+1;
     const taskName = document.createElement("td");
     taskName.textContent = task.TaskName;
     const assignee = document.createElement("td");
@@ -56,11 +64,15 @@ function createTasks(tasks) {
     endDate.textContent = task.TaskAttributes.EndDate;
     const status = document.createElement("td");
     status.textContent = task.TaskAttributes.Status;
+    const priority = document.createElement("td");
+    priority.textContent = task.TaskAttributes.Priority;
+    row.appendChild(taskindex);
     row.appendChild(taskName);
     row.appendChild(assignee);
     row.appendChild(startDate);
     row.appendChild(endDate);
     row.appendChild(status);
+    row.appendChild(priority);
     table.appendChild(row);
   });
 
