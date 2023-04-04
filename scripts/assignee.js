99e8a2bd3054a0178025dee802e2061be665b3cb
DanielRBech <dbech22@student.aau.dk>
2023-04-04 16:21:43 +0200

    Creation of Assignee.js

---
+++ scripts/assignee.js
@@ -0,0 +1,73 @@
+const assigneesSource = new EventSource("http://localhost:3000/events/Assignee");
+const assigneeForm = document.getElementById("assigneeForm")
+const assigneeFormButton = document.getElementById("assigneeButton")
+
+
+
+assigneeFormButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const data = {
+      assigneeName: assigneeForm.value
+    };
+    try {
+      const response = await fetch("http://localhost:3000/SendAssignee", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+      const assigneeresponseData = await response.json();
+      console.log(assigneeresponseData.status, assigneeresponseData);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  let lastAssigneeMessage = null;
+  
+    assigneesSource.addEventListener("message", function getAssignees(event) {
+      if (event.target === assigneesSource) {
+        const assignees = JSON.parse(event.data);
+      if (assignees === lastAssigneeMessage) {
+        return;
+      }
+      if (document.getElementById("assignee")) {
+        document.getElementById("assignee").remove();
+      }
+      
+      const insertAfterThis = document.querySelector("label[for='assignee']");
+  
+      const selectAssigneeLabel = document.createElement("label");
+      const selectAssignee = document.createElement("select");
+      
+      assignees.forEach((assign) => {
+          checkAttribute = assign.hasOwnProperty('assigneeName')
+          console.log(assign.TaskName)
+          console.log(checkAttribute)
+          if (checkAttribute){
+            const option = document.createElement("option");
+            option.textContent = assign.assigneeName;
+            option.value = assign.assigneeName;
+            console.log(option)
+            selectAssignee.appendChild(option)   
+          }
+          else {
+            selectAssignee.remove()
+            selectAssigneeLabel.remove()
+            return;
+          } 
+      });
+  
+      selectAssigneeLabel.textContent = "Assignee";
+      selectAssignee.id = "assignee";
+      selectAssignee.required = true;
+      selectAssignee.name = "assignee";
+  
+      insertAfterThis.insertAdjacentElement("afterend", selectAssignee);
+      lastAssigneeMessage = assignees;
+      }
+    else {
+  
+      return
+    }
+  })
