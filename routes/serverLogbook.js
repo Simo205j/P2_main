11d8b6282bd778cd742ee7defda85fe4dbad7b72
DanielRBech <dbech22@student.aau.dk>
2023-04-14 18:54:40 +0200

    Pas2

--- routes/serverLogbook.js
+++ routes/serverLogbook.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const DataStore = require("nedb");
-const bodyParser = require('body-parser');
-router.use(bodyParser.json());
 
 const logbookDataBase = new DataStore({ filename: "./Databases/logbookDataBase.db", autoload: true });
 logbookDataBase.loadDatabase();
@@ -14,6 +12,7 @@ router.get('/logbook', (req, res) => {
 });
 
 // Additional logbook routes can be defined here
+
 router.delete("/Delete", (req, res) => {
     const data =  req.body._id
     console.log("Deleting logbook with _id:", data);
@@ -28,45 +27,14 @@ router.delete("/Delete", (req, res) => {
   });
   
   });
-
-router.patch("/EditEntry", (req, res) => {
-  const data = req.body;
-  console.log("Deleting logbook entry with _id:", data._id, "at index:", data.index);
-
-  // Load the logbook entry from the database using _id
-  logbookDataBase.findOne({ _id: data._id }, (err, logbookEntry) => {
-    if (err) {
-      console.error("Error finding logbook entry:", err);
-      res.status(500).send({ error: err });
-    } else {
-      // Delete the specified index from paragraphs, status, and headers arrays
-      logbookEntry.paragraphs.splice(data.index, 1);
-      logbookEntry.status.splice(data.index, 1);
-      logbookEntry.headers.splice(data.index, 1);
-
-      // Save the updated logbook entry back to the database
-      logbookDataBase.update({ _id: data._id }, logbookEntry, {}, (err, numReplaced) => {
-        if (err) {
-          console.error("Error updating logbook entry:", err);
-          res.status(500).send({ error: err });
-        } else {
-          console.log("Deleted logbook entry at index:", data.index);
-          res.status(200).json({ status: "success", message: "Logbook entry deleted" });
-        }
-      });
-    }
-  });
-});
   
-
-
   router.post("/UpdatePost", (req, res) => {
     logbookEntry = req.body; 
     console.log(req.body)
     console.log("Got request to POST new logbook:" + logbookEntry._id);
     logbookDataBase.update(
       { _id: logbookEntry._id }, // query to find the document
-      { $push: { paragraphs: logbookEntry.paragraphs, headers: logbookEntry.headers , assignee: logbookEntry.assignee} }, // update operation to insert new element
+      { $push: { paragraphs: logbookEntry.paragraphs, headers: logbookEntry.headers } }, // update operation to insert new element
       {}, // options (empty in this case)
       function (err, numReplaced, upsert) {
         if (err) {
@@ -74,8 +42,8 @@ router.patch("/EditEntry", (req, res) => {
           res.status(500)
         } else {
           console.log('Updated', numReplaced, 'documents');
-          res.status(200).json({ paragraphs: logbookEntry.paragraphs, headers: logbookEntry.headers , assignee: logbookEntry.assignee})
-        }
+          res.status(200)
+        } 
       }
     );
   });
@@ -93,29 +61,12 @@ router.patch("/EditEntry", (req, res) => {
       }
     });
   })
-
-  router.patch("/updateDisplayStatus", (req, res) => {
-    const data = req.body;
-    console.log("GOT PATCH request to update logbook entry status", data)
-    //SEARCH FOR TASK WITH ID,                        REPLACED ATTRIBUES
-    logbookDataBase.update({_id: data._id}, {$set: { display: data.display} },{}, (err, updatedTask) => {
-      if(err) 
-      {
-        res.status(500).send({ error: err });
-      }
-      else
-      {
-        res.status(200).json({
-          status: "PATCHED Logbook",
-          data: data
-        });
-      }
-    })
-  })
-
+  
+  
   router.patch("/UpdateStatus", (req, res) => {
     const data = req.body;
     console.log("GOT PATCH request to update logbook", data._id)
+    console.log("LINE 248: ", data.status)
     //SEARCH FOR TASK WITH ID,                        REPLACED ATTRIBUES
     logbookDataBase.update({_id: data._id}, {$set: { status: data.status} },{}, (err, updatedTask) => {
       if(err) 
