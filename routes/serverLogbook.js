1c214534c014f4afc8f03e1acd3d4f72784fcdd8
MertzAndreas <SG82DA@student.aau.dk>
2023-04-15 15:32:57 +0200

    Logbook beta 0.1

--- routes/serverLogbook.js
+++ routes/serverLogbook.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const DataStore = require("nedb");
+const bodyParser = require('body-parser');
+router.use(bodyParser.json())
 
 const logbookDataBase = new DataStore({ filename: "./Databases/logbookDataBase.db", autoload: true });
 logbookDataBase.loadDatabase();
@@ -61,88 +63,108 @@ router.delete("/Delete", (req, res) => {
       }
     );
   });
-  router.post('/SendLogbook', (req, res) => {
-    console.log("Got POST submitLogbook request", req.body);
-    const data = req.body;
-    logbookDataBase.insert(data, (err, newLogbook) => {
-      if (err) {
-        res.status(500).send({ error: err });
-      } 
-      else {
-        res.status(200).json({
-          LogbookName: newLogbook.LogbookName,
-        });
-      }
-    });
-  })
-  
-  
-  router.patch("/UpdateStatus", (req, res) => {
-    const data = req.body;
-    console.log("GOT PATCH request to update logbook", data._id)
-    console.log("LINE 248: ", data.status)
-    //SEARCH FOR TASK WITH ID,                        REPLACED ATTRIBUES
-    logbookDataBase.update({_id: data._id}, {$set: { status: data.status} },{}, (err, updatedTask) => {
-      if(err) 
-      {
-        res.status(500).send({ error: err });
-      }
-      else
-      {
-        res.status(200).json({
-          status: "PATCHED Logbook",
-          data: data
-        });
-      }
-    });
+
+router.patch("/SaveLogbookEntry", (req, res) => {
+  const data = req.body;
+  console.log("GOT PATCH request to update logbook", data)
+            
+  logbookDataBase.update({_id: data._id}, {$set: {HeaderArray: data.HeaderArray, ParagraphArray: data.ParagraphArray, CheckboxArray: data.CheckboxArray} },{}, (err, updatedTask) => {
+    if(err) 
+    {
+      res.status(500).send({ error: err });
+    }
+    else
+    {
+      res.status(200).json({
+        status: "PATCHED Logbook",
+        data: data
+      });
+    }
+  });
+});
+
+router.post('/SendLogbook', (req, res) => {
+  console.log("Got POST submitLogbook request", req.body);
+  const data = req.body;
+  logbookDataBase.insert(data, (err, newLogbook) => {
+    if (err) {
+      res.status(500).send({ error: err });
+    } 
+    else {
+      res.status(200).json({
+        LogbookName: newLogbook.LogbookName,
+      });
+    }
+  });
+})
+
+
+router.patch("/UpdateStatus", (req, res) => {
+  const data = req.body;
+  console.log("GOT PATCH request to update logbook", data._id)
+  console.log("LINE 248: ", data.status)
+  //SEARCH FOR TASK WITH ID,                        REPLACED ATTRIBUES
+  logbookDataBase.update({_id: data._id}, {$set: { status: data.status} },{}, (err, updatedTask) => {
+    if(err) 
+    {
+      res.status(500).send({ error: err });
+    }
+    else
+    {
+      res.status(200).json({
+        status: "PATCHED Logbook",
+        data: data
+      });
+    }
+  });
+});
+
+router.get('/events', eventsHandlerLogbooks);
+
+function eventsHandlerLogbooks(request, response) {
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+  };
+  response.writeHead(200, headers);
+
+  const clientId = Date.now();
+
+  const newClient = {
+    id: clientId,
+    response
+  };
+  clients.push(newClient);
+  console.log("New client: " + clientId);
+
+  // Immediately send the current task data to the new client
+  logbookDataBase.find({},{_id: 1, date: 1}, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.write(`data: ${JSON.stringify(data)}\n\n`);
+    }
   });
   
-  router.get('/events', eventsHandlerLogbooks);
-  
-  function eventsHandlerLogbooks(request, response) {
-    const headers = {
-      'Content-Type': 'text/event-stream',
-      'Connection': 'keep-alive',
-      'Cache-Control': 'no-cache'
-    };
-    response.writeHead(200, headers);
-  
-    const clientId = Date.now();
-  
-    const newClient = {
-      id: clientId,
-      response
-    };
-    clients.push(newClient);
-    console.log("New client: " + clientId);
-  
-    // Immediately send the current task data to the new client
-    logbookDataBase.find({},{_id: 1, date: 1}, (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        response.write(`data: ${JSON.stringify(data)}\n\n`);
-      }
-    });
-    
-    request.on('close', () => {
-      console.log(`${clientId} Connection closed`);
-      clients = clients.filter(client => client.id !== clientId);
-    });
-  }
-  let lastSentDataLogbook = null;
-  function sendEventsToAlle() {
-    logbookDataBase.find({}, (err, data) => {
-      if (err) {
-        console.log(err);
-      } else if (JSON.stringify(data) !== JSON.stringify(lastSentDataLogbook)) {
-        console.log(data);
-        clients.forEach(client => client.response.write(`data: ${JSON.stringify(data)}\n\n`));
-        lastSentDataLogbook = data;
-      }
-    });
-  }
-  
-  setInterval(sendEventsToAlle, 1000); // sends events to all clients every 10 seconds
-  
-  module.exports = router;
+  request.on('close', () => {
+    console.log(`${clientId} Connection closed`);
+    clients = clients.filter(client => client.id !== clientId);
+  });
+}
+let lastSentDataLogbook = null;
+function sendEventsToAlle() {
+  logbookDataBase.find({}, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else if (JSON.stringify(data) !== JSON.stringify(lastSentDataLogbook)) {
+      console.log(data);
+      clients.forEach(client => client.response.write(`data: ${JSON.stringify(data)}\n\n`));
+      lastSentDataLogbook = data;
+    }
+  });
+}
+
+setInterval(sendEventsToAlle, 1000); // sends events to all clients every 10 seconds
+
+module.exports = router;
